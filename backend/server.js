a954847d25997b0fef234b306ee3f168ec0e814c
Gatik Yadav <gatikyadav8205@gmail.com>
2025-07-02 18:33:30 +0530

    few more changes

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname,'frontend')));
 
 app.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, 'public', 'login.html'));
+   res.sendFile(path.join(__dirname, 'frontend', 'login.html'));
 });
 
 mongoose.connect(process.env.MONGO_URI)
