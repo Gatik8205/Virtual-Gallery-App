821a80496349d8c8ab87d2be0660deb9db01d8ea
Gatik Yadav <gatikyadav8205@gmail.com>
2025-07-11 16:50:14 +0530

    forgot password bug fixed

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ mongoose.connect(process.env.MONGO_URI)
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: parseInt(process.env.EMAIL_PORT),
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
