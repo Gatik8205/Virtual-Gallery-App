5b4100eecacaed0ff1759e60f41aa18931b3327b
Gatik Yadav <gatikyadav8205@gmail.com>
2026-04-28 12:19:07 +0530

    redesigned ui

--- frontend/index.js
+++
@@ -1,9 +0,0 @@
-if (window.location.search.includes("loggedout=true")) {
-      showMessage("👋 Logged out successfully!", "success");
-      window.history.replaceState(null, '', window.location.pathname);
-    }
-    if (!isLoggedIn()) {
-      window.location.href = "login.html";
-    }
-    const logoutBtn = document.getElementById("logoutBtn");
-    logoutBtn.addEventListener("click", logout);
